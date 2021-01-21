a4805570296d79f4ee58bea8230ddb6d03988972
mschulte <maximilliangschulte@pm.me>
2021-01-21 10:43:28 -0600

    successfully returning data to the c api for bimax of binary matrix

--- bimax.go
+++ bimax.go
@@ -7,51 +7,51 @@ import (
 
 	"github.com/yourbasic/graph"
 )
-import "unsafe"
-
-// func main() {}
-
-// func BiMaxBinaryMatrixC(n, m int, data []uint8) **C.GoInt {
+import (
+	"reflect"
+	"unsafe"
+)
 
 //export BiMaxBinaryMatrixC
-// func BiMaxBinaryMatrixC(n, m int, data []uint8) (C.size_t, C.size_t, **C.longlong) {
-// func BiMaxBinaryMatrixC(n, m int, data []uint8) (C.size_t, **C.longlong) {
-func BiMaxBinaryMatrixC(n, m int, data []uint8) (C.size_t, *C.longlong) {
-	rows, cols := BiMaxBinaryMatrix(n, m, data)
-	fmt.Printf("rows = %+v\n", rows)
-	fmt.Printf("cols = %+v\n", cols)
-	// p = C.malloc(C.size_t(len(result)) * C.size_t(unsafe.Sizeof(uintptr(0))))
+func BiMaxBinaryMatrixC(nc, nm int64, input *C.char) (C.size_t, *C.longlong, C.size_t, *C.longlong) {
+	n := int(nc)
+	m := int(nc)
+	// Convert C input data into Go data
+	var data []uint8
+	dataH := (*reflect.SliceHeader)(unsafe.Pointer(&data))
+	dataH.Data = uintptr(unsafe.Pointer(input))
+	dataH.Len = n * m
 
-	size := len(rows) + len(cols)
-	// allocate the *C.double array
-	dataC := C.malloc(C.size_t(size) * C.size_t(unsafe.Sizeof(int64(0))))
+	result := BiMaxBinaryMatrix(n, m, data)
+	return result.ToC()
+}
 
-	// convert the pointer to a go slice so we can index it
-	doubles := (*[1<<30 - 1]C.longlong)(dataC)[:size:size]
-	offset := 0
-	for i, vv := range [][]int{rows, cols} {
-		for j, v := range vv {
-			v64 := int64(v)
-			doubles[i*offset+j] = *(*C.longlong)(unsafe.Pointer(&v64))
-		}
-		offset += len(vv)
+func separateRowCols(A, B *SetOp) (rows, cols []int) {
+	rowset, colset := A, B
+	if B.Has(0) {
+		rowset, colset = B, A
 	}
-
-	// return C.size_t(len(result[0])), C.size_t(len(result[1])), (*C.double)(p)
-	return C.size_t(size), (*C.longlong)(dataC)
+	rows = rowset.Values()
+	cols = colset.Values()
+	// Subtract the number of rows from columns to go from graph column index to
+	// column index
+	for i := 0; i < len(cols); i++ {
+		cols[i] -= rowset.Card()
+	}
+	return
 }
 
 // BiMaxBinaryMatrix takes in an n by m binary matrix where n is the number of
 // rows and m the number of columns.  The data for the binary matrix is
 // specified as a slice of uint8 containing only 1's and 0's.
-//export BiMaxBinaryMatrix
-func BiMaxBinaryMatrix(n, m int, data []uint8) ([]int, []int) {
-	fmt.Printf("data = %+v\n", data)
+func BiMaxBinaryMatrix(n, m int, data []uint8) *BiMaxResult {
 	if (len(data) / m) != n {
 		panic(fmt.Sprintf("matrix data cannot be reshaped into [%d, %d]", n, m))
 	}
+	fmt.Printf("n = %+v\n", n)
+	fmt.Printf("m = %+v\n", m)
 	G := graph.New(n + m)
-	uu, vv := NewSet(), NewSet()
+	U, V := NewSet(), NewSet()
 	for i, x := range data {
 		if x == 0 {
 			continue
@@ -60,45 +60,68 @@ func BiMaxBinaryMatrix(n, m int, data []uint8) ([]int, []int) {
 		graphIdxRow := i / m
 		graphIdxCol := i%m + n
 		// Add to graph and each bipartite vertex set.
-		uu.Add(graphIdxRow)
-		vv.Add(graphIdxCol)
+		U.Add(graphIdxRow)
+		V.Add(graphIdxCol)
 		G.AddBoth(graphIdxRow, graphIdxCol)
 	}
 	// Get the result of the bimax Function
-	A, B := BiMax(G, uu, vv)
-	// Find which contains rows
-	rowset, colset := A, B
-	if B.Has(0) {
-		rowset, colset = B, A
-	}
-	rows := rowset.Values()
-	cols := colset.Values()
-	// Subtract the number of rows from columns to go from graph column index to
-	// column index
-	for i := 0; i < len(cols); i++ {
-		cols[i] -= n
-	}
-	return rows, cols
+	return BiMax(G, U, V)
 }
 
-// func BiMaxVerticies(uu, vv []int) (Set, Set) {
-// 	if len(uu) != len(vv) {
-// 		panic(fmt.Sprintf("expected uu and vv to be the same length got len(uu): %d len(vv) %d", len(uu), len(vv)))
-// 	}
-// 	G := graph.New(len(uu))
-// 	for i := 0; i < len(uu); i++ {
-// 		G.AddBoth(uu[i], vv[i])
-// 	}
-// 	A, B := NewSetFromSlice(uu), NewSetFromSlice(vv)
-// 	if A.Card() < B.Card() {
-// 		return BiMax(B, A, PU)
-// 	}
-// 	return BiMax(A, B, PU)
-// }
+func BiMaxBinaryVerticis(uu, vv []int) *BiMaxResult {
+	if len(uu) != len(vv) {
+		panic(fmt.Sprintf("len(uu): %d len(vv): %d must be equal", len(uu), len(vv)))
+	}
+	U, V := NewSetFromSlice(uu), NewSetFromSlice(vv)
+	G := graph.New(U.Card() + V.Card())
+	for i := 0; i < len(uu); i++ {
+		G.AddBoth(uu[i], vv[i]+U.Card())
+	}
+	return BiMax(G, U, V)
+}
+
+// BiMaxResult represents the result returned from 'BiMax' as a set of boths
+// rows and columns or as the set of two vertecies in a the maximal biclique of
+// graph G.
+type BiMaxResult struct {
+	Rows, Cols *SetOp
+}
+
+// ToC converts the BiMaxResult to allocted data in C so for ease of use in
+// returning results functions 'BiMaxBinaryMatrixC' and 'BiMaxBinaryVerticesC', that
+// deal with returning to the C api.
+func (r *BiMaxResult) ToC() (C.size_t, *C.longlong, C.size_t, *C.longlong) {
+	// getSetValues64 returns the values of a set as []int64.
+	getSetValues64 := func(set *SetOp) []int64 {
+		result := make([]int64, 0, set.Card())
+		set.Each(func(v int) (done bool) {
+			result = append(result, int64(v))
+			return
+		})
+		return result
+	}
+	// Get the values of row and column sets in the result as []int64.
+	rows := getSetValues64(r.Rows)
+	cols := getSetValues64(r.Cols)
+	// toArrC convert slice of int64to a pointer to allocated C.longlong memory
+	// and the size of length of the allocated memory measured in C.longlong.
+	toArrC := func(x []int64) (C.size_t, *C.longlong) {
+		bb := *(*[]byte)(unsafe.Pointer(&x))
+		bbH := (*reflect.SliceHeader)(unsafe.Pointer(&bb))
+		bbH.Len = len(x) * int(unsafe.Sizeof(x[0]))
+		return C.size_t(len(x)), (*C.longlong)(C.CBytes(bb))
+	}
+	// Get the values of rows and cols as C memory pointers to C.longlong data and
+	// lengths of said pointers to data
+	lenRowsC, dataRowsC := toArrC(rows)
+	lenColsC, dataColsC := toArrC(cols)
+
+	return lenRowsC, dataRowsC, lenColsC, dataColsC
+}
 
 // BiMax finds the maximal bipartitie clique of a bipartite graph of graph G
-// where G is a bipartite graph of (U ∪ V, E)
-func BiMax(G *graph.Mutable, L, PU *UnorderedSet) (*SetOp, *SetOp) {
+// where G is a bipartite graph of (U ∪ V, E(G))
+func BiMax(G *graph.Mutable, L, PU *UnorderedSet) *BiMaxResult {
 	// L: is a set of verticies ∈ U that are common neigbors of R; initially L = U
 	// R: is a set of verticies ∈ V belonging to the current biclique; initially
 	// empty
@@ -113,11 +136,11 @@ func BiMax(G *graph.Mutable, L, PU *UnorderedSet) (*SetOp, *SetOp) {
 	Q := NewSet()
 
 	// Resulting sets
-	A, B := NewSet(), NewSet()
+	Rows, Cols := NewSet(), NewSet()
 
 	var bicliqueFind func(P *OrderedSet, L, R, Q *UnorderedSet)
 	bicliqueFind = func(P *OrderedSet, L, R, Q *UnorderedSet) {
-		for i := 0; 0 < P.Card(); i++ {
+		for i := 0; i < P.Card(); i++ {
 			x := P.Get(0)
 
 			// Candidates
@@ -175,12 +198,17 @@ func BiMax(G *graph.Mutable, L, PU *UnorderedSet) (*SetOp, *SetOp) {
 					}
 					return
 				})
+				// TODO: might be able to optimize based on number of enumerated
+				// bicliques <20-01-21, Max Schulte> //
+
 				// Print Maximal biclique
-				if (A.Card() * B.Card()) < (Lʹ.Card() * Rʹ.Card()) {
+				// fmt.Println(Lʹ, Rʹ)
+
+				if (Rows.Card() * Cols.Card()) < (Lʹ.Card() * Rʹ.Card()) {
 					// A = Lʹ.Copy()
 					// B = Rʹ.Copy()
-					A = Lʹ
-					B = Rʹ
+					Rows = Lʹ
+					Cols = Rʹ
 				}
 				// fmt.Println(Lʹ, Rʹ)
 				if Pʹ.Card() == 0 {
@@ -193,7 +221,8 @@ func BiMax(G *graph.Mutable, L, PU *UnorderedSet) (*SetOp, *SetOp) {
 		}
 	}
 	bicliqueFind(P, L, R, Q)
-	return &SetOp{A}, &SetOp{B}
+	result := BiMaxResult{&SetOp{Rows}, &SetOp{Cols}}
+	return &result
 }
 
 // ClosedDegree returns the degree of the closed neighborhood at v
